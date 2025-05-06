d68b64702410ea56e612b3eb61248738d33fae03
hrishob <hrishobp@gmail.com>
2025-05-06 16:04:00 +0530

    done

---
+++ backend/controllers/otpControll.js
@@ -0,0 +1,9 @@
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "hrishobp@gmail.com",
+      pass: "qfni tnqw bldi jhug"
+    }
+  });
+  
